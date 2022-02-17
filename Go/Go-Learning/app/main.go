3ff76e8b25c3ea7611e18379c238b64b68698287
kakariko3 <kakariko01@gmail.com>
2022-02-17 14:03:39 +0900

    Edit main.go

--- Go/Go-Learning/app/main.go
+++ Go/Go-Learning/app/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type Person struct {
+	firstName string
+	age       int
+}
+
 func main() {
-    fmt.Println("Hello World!")
+	var mike Person
+	mike.firstName = "Mike"
+	mike.age = 20
+	fmt.Println(mike) // => {Mike 20}
+
+	bob := Person{"Tim", 30}
+	fmt.Println(bob) // => {Tim 30}
+
+	sam := Person{age: 15, firstName: "Sam"}
+	fmt.Println(sam) // => {Sam 15}
+
+	tim := new(Person)
+	tim.firstName = "Bob"
+	tim.age = 25
+	fmt.Println(tim) // => &{Bob 25}
 }
